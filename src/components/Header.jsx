0284b913d45189de43f0d379b80a403a5fd9daaa
Tahsinul islam <mohammadsiam8760@gmail.com>
2025-06-25 02:26:59 +0600

    added header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from "../assets/logo.png";
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3 text-gray-500'>
-            <img className='w-[350px]' src={logo} alt="" />
-            <p>Journalism without Fear or favour</p>
+            <img className='w-[400px]' src={logo} alt="" />
+            <p className='text-accent'>Journalism without Fear or favour</p>
+             <p className='text-semibold text-accent'>{format(new Date(),"EEEE, MMMM MM yyyy")}</p>
         </div>
     );
 };
