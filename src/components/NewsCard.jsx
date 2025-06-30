42aee83188634271bd2ab4d09d2578fb0c63be17
Tahsinul islam <mohammadsiam8760@gmail.com>
2025-07-01 01:29:11 +0600

    display news card

---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsCard = () => {
+    return (
+        <div>
+            newscard
+        </div>
+    );
+};
+
+export default NewsCard;
--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams, useRouteLoaderData } from 'react-router';
+import NewsCard from '../components/NewsCard';
 
 const CategoryNews = () => {
     const {id}= useParams();
@@ -24,7 +25,12 @@ const [categoryNews, setCategoryNews]=useState([]);
     },[data,id])
     return (
         <div>
-            <h1>Total {categoryNews.length} found</h1>
+            <h2 className='font-bold mb-5'>Total <span className='text-secondary'>{categoryNews.length} </span>news found</h2>
+            <div className='grid grid-cols-1 gap-5'>
+                {
+                    categoryNews.map(news=> <NewsCard></NewsCard>)
+                }
+            </div>
         </div>
     );
 };
