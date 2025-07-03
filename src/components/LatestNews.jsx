9d1e918ca2314b342fb1bb5e8d14cb581040bc70
Tahsinul islam <mohammadsiam8760@gmail.com>
2025-07-04 00:51:57 +0600

    implement Login and register page

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -6,7 +6,7 @@ const LatestNews = () => {
         <div className='flex items-center gap-5 bg-base-200 p-3'>
             <p className='text-base-100 bg-secondary px-2 py-3'>Latest</p>
             <Marquee className='flex 'pauseOnHover={true}>
-                <p className='font-bold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, unde.</p>
+                <p className='font-bold'>France has banned smoking in public places like beaches and parks. Any smoker who lights up a cigarette in public will be breaking the law. The new smoking ban is to protect children from passive smoking. This happens when you breathe in smoke from other people who are smoking..</p>
                 
             </Marquee>
             
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { NavLink } from 'react-router';
+import { Link, Links, NavLink } from 'react-router';
 import user from "../assets/user.png"
+
 
 const Navbar = () => {
     return (
@@ -13,7 +14,7 @@ const Navbar = () => {
             </div>
             <div className="login-btn flex gap-5 items-center">
                 <img src={user} alt="" />
-                <button className='btn btn-primary px-10'>Login</button>
+            <Link to="/auth/login" className='btn btn-primary px-10'>Login</Link>
             </div>
         </div>
     );
