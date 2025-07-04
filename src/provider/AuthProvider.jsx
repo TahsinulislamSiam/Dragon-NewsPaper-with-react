d6811915961346a7608b5d65b39c8ce9256f8838
Tahsinul islam <mohammadsiam8760@gmail.com>
2025-07-04 23:12:37 +0600

    set up context and provider

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,13 +1,19 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 export const AuthContext = createContext();
 
-const AuthProvider=()=>{
-    
-}
 
-const AuthProvider = () => {
 
-    return <AuthContext></AuthContext>;
+const AuthProvider = ({children}) => {
+    const [user,setUser]=useState({
+        name:"Mohammad siam",
+        email:"mohammadsiam8760@gmail.com",
+    });
+    const authData= {
+        user,
+        setUser,
+
+    };
+    return <AuthContext value={authData}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
