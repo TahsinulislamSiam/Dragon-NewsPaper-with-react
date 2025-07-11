7f98f031da69a980b884dfc1ad7fa7e66415cd49
Tahsinul islam <mohammadsiam8760@gmail.com>
2025-07-11 22:30:42 +0600

    implement loading feature

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -6,16 +6,18 @@ import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWith
 const auth = getAuth(app);
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null);
+    const [loading,setLoading]=useState(true);
 
     console.log(user);
 
 const createUser=(email,password)=>{
-
+    setLoading(true);
     return createUserWithEmailAndPassword(auth,email,password)
 
 };
 
 const signIn=(email,password)=>{
+    setLoading(true);
     return signInWithEmailAndPassword(auth,email,password);
 }
 
@@ -27,6 +29,7 @@ const logOut=()=>{
 useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth,(currentUser)=>{
         setUser(currentUser);
+        setLoading(false);
       });
       return ()=>{
           unsubscribe();  
@@ -39,6 +42,8 @@ useEffect(()=>{
         createUser,
         logOut,
         signIn,
+        loading,
+        setLoading,
 
     };
     return <AuthContext value={authData}>{children}</AuthContext>;
